906fd35c5a34e9364e29519b7faf47c1492f0ba0
Jason Adams <j.adams@f5.com>
2023-12-04 09:14:17 -0800

    Day 2 - Task 2

--- 2023/02/task1.py
+++ 2023/02/task1.py
@@ -3,37 +3,59 @@
 import re
 
 # only 12 red cubes, 13 green cubes, and 14 blue cubes
-max_cubes = {
+MAX_CUBES = {
     "red": 12,
     "green": 13,
     "blue": 14
 }
 
-def task_1():
-    total = 0
-    # [1] = Game Number
-    # [2] = Game Results
-    pattern = '^Game\s+(\d+):\s+(.*)$'
-    with open("2023/02/input", mode="r") as input:
-        for line in input.readlines():
-            impossible_game = False
-            match = re.match(pattern, line)
-            game_number = int(match[1])
-            match_results = match[2].split(";")
-            match_results = [s.strip() for s in match_results]
-            for set in match_results:
-                if impossible_game == True:
-                    break
-                for h in set.split(","):
-                    count, color = h.strip().split(" ", 1)
-                    count = int(count)
-                    if count > max_cubes.get(color):
-                        impossible_game = True
-                        break
-            if impossible_game == True:
-                continue
-            total += game_number
-    return total
+PATTERN = '^Game\s+(\d+):\s+(.*)$'
+
+def split_line(line: str):
+    # match[1] = Game Number
+    # match[2] = Game Results
+    match = re.match(PATTERN, line)
+    game_number = int(match[1])
+    match_results = match[2].split(";")
+    match_results = [s.strip() for s in match_results]
+
+    return game_number, match_results
+
+def task_1(line: str):
+    game_number, match_results = split_line(line)
+    for set in match_results:
+        for h in set.split(","):
+            count, color = h.strip().split(" ", 1)
+            count = int(count)
+            if count > MAX_CUBES.get(color):
+                return 0
+    return game_number
+
+def task_2(line):
+    min_required = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+    game_number, match_results = split_line(line)
+    for set in match_results:
+        for h in set.split(","):
+            count, color = h.strip().split(" ", 1)
+            count = int(count)
+            if count > min_required.get(color):
+                min_required[color] = count
+
+    return min_required.get("red") * min_required.get("green") * min_required.get("blue")
 
 if __name__ == "__main__":
-    print(f"task 1: {task_1()}")
+    total_task_1 = 0
+    total_task_2 = 0
+
+    with open("2023/02/input", mode="r") as input:
+        for line in input.readlines():
+            total_task_1 += task_1(line)
+            total_task_2 += task_2(line)
+    
+
+    print(f"Task 1: {total_task_1}")
+    print(f"Task 2: {total_task_2}")
