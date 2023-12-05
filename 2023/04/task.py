bfdb8c95a63fcb81fd84330b796a8f8eed129c20
Jason Adams <j.adams@f5.com>
2023-12-04 23:17:00 -0800

    Day 4 Initial Commit

---
+++ 2023/04/task.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    total_task_1 = 0
+    total_task_2 = 0
+    input_file = "2023/03/input"
+
+
+print(f"Task 1 Total: {total_task_1}")
+print(f"Task 2 Total: {total_task_2}")
