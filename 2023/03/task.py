aaeffed63ceeb65e9012bbf1a49242973b6c022e
Jason Adams <j.adams@f5.com>
2023-12-04 23:04:33 -0800

    Day 3: Task 2 - Complete

--- 2023/03/task.py
+++ 2023/03/task.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python3
 
+
 def task_1(x: int, y: int) -> None:
     if searched_grid[x][y]:
         return 0
     if is_int(x=x, y=y):
         num = char_grid[x][y]
         lnum = search_left(x=x, y=y)
-        rnum =search_right(x=x, y=y)
+        rnum = search_right(x=x, y=y)
         num = lnum + num + rnum
 
         return int(num)
     return 0
 
+
+def task_2(x: int, y: int) -> None:
+    if searched_grid[x][y]:
+        return 0
+    if is_int(x=x, y=y):
+        num = char_grid[x][y]
+        lnum = search_left(x=x, y=y)
+        rnum = search_right(x=x, y=y)
+        num = lnum + num + rnum
+
+        return int(num)
+    return 0
+
+
 def is_symbol(char: str):
-    return (char.isalnum() == False and char != '.')
+    return char.isalnum() == False and char != "."
+
 
 def create_grids(input: str):
     char_grid = []
     symbol_grid = []
     searched_grid = []
-    
+
     with open(input, mode="r") as input:
         for x, line in enumerate(input, start=0):
             """
@@ -39,8 +55,10 @@ def create_grids(input: str):
 
     return char_grid, symbol_grid, searched_grid
 
+
 def is_int(x: int, y: int) -> bool:
     return char_grid[x][y].isdigit()
+
 
 def get_adjacent_cells(x: int, y: int) -> list:
     return [
@@ -51,8 +69,9 @@ def get_adjacent_cells(x: int, y: int) -> list:
         (x, (y + 1)),
         ((x + 1), (y - 1)),
         ((x + 1), (y)),
-        ((x + 1), (y + 1))
+        ((x + 1), (y + 1)),
     ]
+
 
 def searched(x: int, y: int) -> None:
     """
@@ -64,12 +83,14 @@ def searched(x: int, y: int) -> None:
     except IndexError:
         return None
 
+
 def get_char(x: int, y: int) -> str:
     try:
         char = char_grid[x][y]
     except IndexError:
         return ""
     return char
+
 
 def search_left(x: int, y: int) -> str:
     num = ""
@@ -84,6 +105,7 @@ def search_left(x: int, y: int) -> str:
         searched(x=x, y=y)
     return num
 
+
 def search_right(x: int, y: int):
     num = ""
     y = y + 1
@@ -96,6 +118,7 @@ def search_right(x: int, y: int):
         char = get_char(x=x, y=y)
         searched(x=x, y=y)
     return num
+
 
 if __name__ == "__main__":
     total_task_1 = 0
@@ -111,7 +134,27 @@ if __name__ == "__main__":
                 # ...
                 # .*.
                 # ...
-                total_task_1 += sum([task_1(x=cx, y=cy) for cx, cy in get_adjacent_cells(x=x, y=y)])
-                    
+                total_task_1 += sum(
+                    [task_1(x=cx, y=cy) for cx, cy in get_adjacent_cells(x=x, y=y)]
+                )
+                t2 = [task_2(x=cx, y=cy) for cx, cy in get_adjacent_cells(x=x, y=y)]
+                # Task 2: This should be bubbled up into def task_2(); but this works and it's late.
+                for i, l in enumerate(t2):
+                    if i == 0:
+                        num_ints = 0
+
+                    if l > 0:
+                        num_ints += 1
+                        if num_ints == 1:
+                            num_1 = l
+                        if num_ints == 2:
+                            num_2 = l
+
+                    if i == 7:
+                        if num_ints == 2:
+                            total_task_2 += num_1 * num_2
+                        num_1 = 0
+                        num_2 = 0
+
 print(f"Task 1 Total: {total_task_1}")
 print(f"Task 2 Total: {total_task_2}")
