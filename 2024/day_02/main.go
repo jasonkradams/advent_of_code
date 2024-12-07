50a83cf5d55762b9e014286c69399e18a68361ea
Jason Adams <j.adams@f5.com>
2024-12-06 22:39:20 -0800

    Day 2 complete

--- 2024/day_02/main.go
+++ 2024/day_02/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoc/file"
+
 	"bufio"
 	"fmt"
 	"math"
@@ -24,6 +25,12 @@ type Report struct {
 	safe         bool
 }
 
+func NewReport() Report {
+	return Report{
+		safe: true,
+	}
+}
+
 func checkIncrementing(report *Report, reading, lastReading int) {
 	if reading > lastReading {
 		report.incrementing = true
@@ -36,31 +43,36 @@ func checkDecrementing(report *Report, reading, lastReading int) {
 	}
 }
 
-// isSafe marks a report safe if:
+// checkSafety marks a report safe if:
 // The levels are either all increasing or all decreasing.
 // Any two adjacent levels differ by at least one and at most three.
-func isSafe(report Report) bool {
+func checkSafety(report Report) bool {
 	var lastReading int = report.levels[0]
 
 	for i := 1; i < len(report.levels); i++ {
+
 		reading := report.levels[i]
 
 		checkIncrementing(&report, reading, lastReading)
 		checkDecrementing(&report, reading, lastReading)
 
 		if reading == lastReading {
-			return false
+			report.safe = false
+			break
 		}
-
 		if report.incrementing && report.decrementing {
-			return false
+			report.safe = false
+			break
 		}
 		if safeDifference(lastReading, reading) != true {
-			return false
+			report.safe = false
+			break
 		}
+
 		lastReading = reading
 	}
-	return true
+
+	return report.safe
 }
 
 func safeDifference(a, b int) bool {
@@ -76,12 +88,7 @@ func safeDifference(a, b int) bool {
 	return true
 }
 
-func taskOne(reports []Report) int {
-	// Identify and mark all safe reports.
-	for i := range reports {
-		reports[i].safe = isSafe(reports[i])
-	}
-
+func safeCount(reports []Report) int {
 	// Count reports with Safe = true
 	safeCount := 0
 	for _, report := range reports {
@@ -92,8 +99,49 @@ func taskOne(reports []Report) int {
 	return safeCount
 }
 
-func taskTwo() {
+func taskOne(reports []Report) int {
+	// Identify and mark all safe reports.
+	for i := range reports {
+		reports[i].safe = checkSafety(reports[i])
+	}
 
+	safeCount := safeCount(reports)
+	return safeCount
+}
+
+func deleteLevel(levels []int, i int) []int {
+	// Create a copy of the slice to avoid mutating the original
+	copyLevels := append([]int{}, levels...)
+	return append(copyLevels[:i], copyLevels[i+1:]...)
+}
+
+func taskTwo(reports []Report) int {
+	for i, report := range reports {
+		if safe := checkSafety(report); safe {
+			reports[i].safe = true
+			break
+		}
+
+		// reset safety to true to give us another chance.
+		report.safe = true
+
+		// Try every combination of removing a single reading from the report
+		// i.e. does any combination of full report, or full report -1 result in a pass?
+		// logger.Printf("%d:\treport: %v", i, report.levels)
+		for j := range report.levels {
+			newReport := report
+			// Always work on the original levels, not mutated ones
+			newReport.levels = deleteLevel(newReport.levels, j)
+
+			if safe := checkSafety(newReport); safe {
+				reports[i].safe = true
+				break
+			}
+		}
+	}
+
+	safeCount := safeCount(reports)
+	return safeCount
 }
 
 func buildReports(file *os.File) []Report {
@@ -111,18 +159,25 @@ func buildReports(file *os.File) []Report {
 
 			readings = append(readings, reading)
 		}
-		reports = append(reports, Report{levels: readings})
+
+		report := NewReport()
+		report.levels = readings
+
+		reports = append(reports, report)
 	}
 	return reports
 }
 
 func main() {
+
 	// Open the file
 	file := file.Get(filePath)
 	defer file.Close()
-
 	reports := buildReports(file)
-	safeCount := taskOne(reports)
 
-	fmt.Println("task 1: ", safeCount)
+	taskOneResult := taskOne(reports)
+	taskTwoResult := taskTwo(reports)
+
+	fmt.Println("task 1: ", taskOneResult)
+	fmt.Println("task 2: ", taskTwoResult)
 }
--- 2024/day_02/main_test.go
+++ 2024/day_02/main_test.go
@@ -9,16 +9,31 @@ var fileName string = "input.txt"
 
 func TestTaskOne(t *testing.T) {
 	t.Run("task 1", func(t *testing.T) {
-		// Open the file
-		file := file.Get(fileName)
-		defer file.Close()
-		reports := buildReports(file)
-
-		// safeCount := taskOne(reports)
+		reports := buildTestReports()
 		got := taskOne(reports)
 		want := 670
 		if got != want {
 			t.Errorf("got %d want %d", got, want)
 		}
 	})
+}
+
+func TestTaskTwo(t *testing.T) {
+	t.Run("task 2", func(t *testing.T) {
+		reports := buildTestReports()
+		taskOne(reports)
+		got := taskTwo(reports)
+		want := 700
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+}
+
+func buildTestReports() []Report {
+	file := file.Get(fileName)
+	defer file.Close()
+	reports := buildReports(file)
+
+	return reports
 }
