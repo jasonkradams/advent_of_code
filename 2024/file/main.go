eade1bfa605292d4c22d496662bc98ab4598cfb2
Jason Adams <j.adams@f5.com>
2024-12-08 22:40:44 -0800

    Add godoc for file.Get()

--- 2024/file/main.go
+++ 2024/file/main.go
@@ -5,6 +5,24 @@ import (
 	"os"
 )
 
+// Get opens the specified file and returns a pointer to the opened *os.File.
+// If the file cannot be opened, it logs the error message and panics.
+//
+// Parameters:
+//   - file: A string representing the path to the file to be opened.
+//
+// Returns:
+//   - A pointer to the opened *os.File.
+//
+// Panics:
+//   - If the file cannot be opened, the function logs the error message and terminates the program.
+//
+// Example:
+//
+//	file := file.Get("input.txt")
+//	defer file.Close()
+//
+//	// Use the file for reading...
 func Get(file string) *os.File {
 	contents, err := os.Open(file)
 	if err != nil {
