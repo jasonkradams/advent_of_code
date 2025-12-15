7f8d1fab6dc150fa2b632f4d05f2c2871a6171fc
Jason Adams <jason.k.r.adams@gmail.com>
2025-12-15 09:48:27 -0800

    feat(2025/01/01): 2025 Day 1 Part 2 - Complete

--- 2025/day01/day01.go
+++ 2025/day01/day01.go
@@ -8,68 +8,102 @@ import (
 )
 
 const (
-	zeroPoint  = 0
-	startPoint = 50
-	maxPoint   = 99
+	zeroPoint   = 0
+	startPoint  = 50
+	maxPoint    = 99
+	totalPoints = 100
 )
 
-func rotateLeft(point, distance int) int {
-	distance = distance % 100
-	point -= distance
+type Answer struct {
+	LandOnZero     int
+	ClicksPastZero int
+}
+
+type Rotation struct {
+	point    int
+	distance int
+}
+
+func NewAnswer(rotations []string) *Answer {
+	return &Answer{
+		LandOnZero:     0,
+		ClicksPastZero: 0,
+	}
+}
+
+func (a *Answer) rotateLeft(r Rotation) int {
+	a.ClicksPastZero += r.distance / totalPoints
+	r.distance = r.distance % totalPoints
+
+	point := r.point - r.distance
+
 	if point < zeroPoint {
+		if r.point != 0 {
+			a.ClicksPastZero++
+		}
 		point = (maxPoint + 1 + point)
 	}
-	return point
-}
-
-func rotateRight(point, distance int) int {
-	distance = distance % 100
-	point += distance
-	if point > maxPoint {
-		return (point - maxPoint - 1)
+	if point == 0 {
+		a.ClicksPastZero++
 	}
 	return point
-
 }
 
-func Day01(rotations []string) int {
+func (a *Answer) rotateRight(r Rotation) int {
+	a.ClicksPastZero += r.distance / totalPoints
+	distance := r.distance % totalPoints
+
+	point := r.point + distance
+	if point > maxPoint {
+		if r.point != 0 {
+			a.ClicksPastZero++
+		}
+		return (point - maxPoint - 1)
+	}
+	if point == 0 {
+		a.ClicksPastZero++
+	}
+	return point
+}
+
+func Day01(rotations []string) Answer {
+	answer := NewAnswer(rotations)
 	currentPoint := startPoint
-	var code int
 	for _, r := range rotations {
 		direction := string(r[0])
 		distanceS := r[1:]
 		distance, _ := strconv.Atoi(distanceS)
 
-		// fmt.Println(direction, distance)
-
 		switch direction {
 		case "L":
-			fmt.Printf("%qB, %d, distance: %d\n", direction, currentPoint, distance)
-
-			currentPoint = rotateLeft(currentPoint, distance)
-
-			fmt.Printf("%qA, %d, distance: %d\n", direction, currentPoint, distance)
+			b := currentPoint
+			r := answer.ClicksPastZero
+			currentPoint = answer.rotateLeft(Rotation{currentPoint, distance})
+			fmt.Printf("%q: %3.1d < %3.1d < %3.1d (%2.1d)\n", direction, b, distance, currentPoint, (answer.ClicksPastZero - r))
 		case "R":
-			fmt.Printf("%qB, %d, distance: %d\n", direction, currentPoint, distance)
-			currentPoint = rotateRight(currentPoint, distance)
-			fmt.Printf("%qA, %d, distance: %d\n", direction, currentPoint, distance)
+			b := currentPoint
+			r := answer.ClicksPastZero
+			currentPoint = answer.rotateRight(Rotation{currentPoint, distance})
+			fmt.Printf("%q: %3.1d > %3.1d > %3.1d (%2.1d)\n", direction, b, distance, currentPoint, (answer.ClicksPastZero - r))
 		default:
 			fmt.Printf("Unknown direction %q", direction)
 		}
 
 		if currentPoint == 0 {
-			code++
+			answer.LandOnZero++
 		}
 	}
+	fmt.Printf("%#v\n", answer)
 
-	return code
+	return *answer
 }
 
-func PartOne() int {
+func PartOne() Answer {
 	fileName := "input.txt"
 	input := file.Get(fileName)
 	rotations := file.ReadLines(input)
 	answer := Day01(rotations)
-	fmt.Println("Answer:", answer)
+	fmt.Printf("Answer: %#v\n", answer)
+	fmt.Printf("Total: %d\n", answer.ClicksPastZero+answer.LandOnZero)
 	return answer
 }
--- 2025/day01/day01_test.go
+++ 2025/day01/day01_test.go
@@ -1,38 +1,59 @@
 package day01_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/jasonkradams/advent_of_code/2025/day01"
 )
 
-func TestDay01Example(t *testing.T) {
-	rotations := []string{
-		"L68",
-		"L30",
-		"R48",
-		"L5",
-		"R60",
-		"L55",
-		"L1",
-		"L99",
-		"R14",
-		"L82",
-	}
+func TestDay01(t *testing.T) {
+	t.Run("example input clicks past zero 3 times", func(t *testing.T) {
+		rotations := []string{
+			"L68",
+			"L30",
+			"R48",
+			"L5",
+			"R60",
+			"L55",
+			"L1",
+			"L99",
+			"R14",
+			"L82",
+		}
 
-	want := 3
-	got := day01.Day01(rotations)
+		want := day01.Answer{LandOnZero: 3, ClicksPastZero: 6}
+		got := day01.Day01(rotations)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 
-func TestDay01PartOne(t *testing.T) {
-	want := 1086
-	got := day01.PartOne()
+	t.Run("real input lands on 0 1086 times", func(t *testing.T) {
+		const (
+			wantLandOnZero     = 1086
+			wantClicksPastZero = 6268
+		)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		wrongAnswers := []int{
+			6294,
+		}
+		want := wantLandOnZero
+		got := day01.PartOne()
+
+		if got.LandOnZero != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+
+		for _, wrong := range wrongAnswers {
+			if got.ClicksPastZero == wrong {
+				t.Fatalf("already tried wrong answer: %d", got.ClicksPastZero)
+			}
+		}
+
+		if got.ClicksPastZero != wantClicksPastZero {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
 }
